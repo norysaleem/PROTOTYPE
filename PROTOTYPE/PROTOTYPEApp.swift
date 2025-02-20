fa8b80affc31bb94c4ce471f976291eb8c0587bf
Noradin <nory0305@outlook.com>
2025-02-19 22:10:22 -0500

    Initial Commit

---
+++ PROTOTYPE/PROTOTYPEApp.swift
@@ -0,0 +1,17 @@
+//
+//  PROTOTYPEApp.swift
+//  PROTOTYPE
+//
+//  Created by Nory Saleem on 2/19/25.
+//
+
+import SwiftUI
+
+@main
+struct PROTOTYPEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PROTOTYPETests/PROTOTYPETests.swift
@@ -0,0 +1,17 @@
+//
+//  PROTOTYPETests.swift
+//  PROTOTYPETests
+//
+//  Created by Nory Saleem on 2/19/25.
+//
+
+import Testing
+@testable import PROTOTYPE
+
+struct PROTOTYPETests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
